78f08c3ced1481e6e95a2e770bc2627e863399cb
Thanasi Bakis <thanasibakis@gmail.com>
2020-04-01 20:38:36 -0700

    Fixed issue with failed git commit

--- scoremodel.R
+++ scoremodel.R
@@ -1,6 +1,6 @@
-score.plot = function(density.func, score.limits, my.score = 0)
+score.plot <- function(density.func, score.limits, my.score = 0)
 {
-  p = ggplot(score.limits, aes(x = Score)) +
+  p <- ggplot(score.limits, aes(x = Score)) +
     stat_function(fun = density.func) +
     ylab("Density") +
     theme_minimal() +
@@ -11,50 +11,43 @@ score.plot = function(density.func, score.limits, my.score = 0)
       alpha = 0.4,
       xlim = c(0, my.score)
     )
-  
+
   if (my.score == 0)
     return(p)
-  
+
   p + geom_vline(xintercept = my.score,
                  color = "blue",
                  alpha = 0.3)
 }
 
 
-score.percentile = function(density.func, my.score = 0)
+score.percentile <- function(density.func, my.score = 0)
 {
   integrate(density.func, 0, my.score)$value * 100
 }
 
-<<<<<<< HEAD
 
-score.model = function(max.score, mean.score, sd.score, ec.possible = FALSE)
-=======
-score.model = function(max.score,
-                       mean.score,
-                       sd.score,
-                       ec.possible = FALSE)
->>>>>>> 9805e0d6c0664d702579ea3f25f10ad8e37233ba
+score.model <- function(max.score, mean.score, sd.score, ec.possible = FALSE)
 {
   if (!ec.possible)
   {
     scaled.mean = mean.score / max.score
     scaled.var = sd.score ^ 2 / max.score ^ 2
-    
+
     a = scaled.mean * (scaled.mean * (1 - scaled.mean) / scaled.var - 1)
     b = (1 - scaled.mean) * (scaled.mean * (1 - scaled.mean) / scaled.var - 1)
-    
+
     density.func = function(x)
       dbeta(x / max.score, a, b) / max.score
     score.limits = data.frame(Score = c(0, max.score))
   }
-  
+
   else
   {
     density.func = function(x)
       dnorm(x, mean.score, sd.score)
     score.limits = data.frame(Score = c(0, max.score + sd.score))
   }
-  
+
   return(list(density.func, score.limits))
 }
